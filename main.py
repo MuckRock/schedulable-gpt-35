a9703506e2038e54b8f893454eb0113a538e0397
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2024-04-15 13:53:17 -0500

    Add a key to filter on

--- main.py
+++ main.py
@@ -15,11 +15,16 @@ class GPTScheduler(AddOn):
         key_name = self.data.get("key_name")
         prompt = self.data.get("prompt")
         limiter = self.data.get("limiter")
+        filter_key = self.data.get("filter_key")
 
-        documents = self.client.documents.search(
-            f"+project:{proj_id} -data_{key_name}:*"
-        )
-
+        if filter_key is not None:
+            documents = self.client.documents.search(
+                f"+project:{proj_id} -data_{key_name}:* data_{filter_key}"
+            )
+        else:
+            documents = self.client.documents.search(
+                f"+project:{proj_id} -data_{key_name}:*"
+            )
         for i in range(batch_num):
             # Pull out the IDs for a batch of the documents
             doc_ids = [
